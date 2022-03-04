d81ac368e312901ec8bf33f9038c087be4c61f9c
JustBeachy <ajbeach67@gmail.com>
2022-03-03 22:21:35 -0500

    Half of animations in

---
+++ LedgerFLUF/Assets/FrameRate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRate : MonoBehaviour
+{
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = 60;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- LedgerFLUF/Assets/pKeyButton.cs
+++ LedgerFLUF/Assets/pKeyButton.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 //Private key:  c97b246e294b84351c08ab94b5540b2469c9e06a63ab5d717f25144f1a6449c4
 //Public key:  9d5fea9f54e38276ed51ad840bed6a51b61e2c48067f56ee119c2a018c9b887860f9ec6f6a100070be203fcf30efd84633390533b37c0b3e9f8d0715ae15d7fa
@@ -13,6 +14,8 @@ public class pKeyButton : MonoBehaviour
     string privateKey = "c97b246e294b84351c08ab94b5540b2469c9e06a63ab5d717f25144f1a6449c4";
     public Text myText, Keyguess;
     public bool canPress = true;
+    float timer = 0;
+    bool failedFlag = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +25,16 @@ public class pKeyButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (failedFlag)
+            timer += Time.deltaTime;
+
+        if(timer>=2.5f)
+        {
+            timer = 0;
+            failedFlag = false;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        }
     }
 
     public void AddLetter()
@@ -45,6 +57,7 @@ public class pKeyButton : MonoBehaviour
 
             if (!isMatching)
             {
+                failedFlag = true;
                 GetComponent<AudioSource>().Play();//play "wrong" audio
                 Keyguess.text = "Wrong character. Number of characters you entered correctly: " + (guessArray.Length - 1).ToString();
                 Keyguess.color = Color.red;
@@ -55,6 +68,7 @@ public class pKeyButton : MonoBehaviour
 
             if (guessArray.Length == 64 && isMatching)
             {
+                failedFlag = true;
                 Keyguess.text = "I think you cheated!";
 
                 GameObject[] allButtons = GameObject.FindGameObjectsWithTag("Button");
